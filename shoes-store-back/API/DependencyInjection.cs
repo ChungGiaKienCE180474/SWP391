c2e58ea0237314330524e64f91187b19b9444c46
Duy Le <leduy1159@gmail.com>
2024-10-22 02:38:42 +0700

    add backend base

---
+++ shoes-store-back/API/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using API.DbConext;
+
+namespace API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<ShoesDbContext>();
+            return services;
+        }
+
+        public static IServiceCollection AddWebAPI(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+    }
+}
